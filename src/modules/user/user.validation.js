6b4515d0fe04e6f0ef6a759d23c60b70a091751f
Alfian Andre Ramadhan <andrealfian69699@gmail.com>
2026-04-19 17:35:03 +0700

    feat: refactor updateUserSchema for improved readability and maintainability

--- src/modules/user/user.validation.js
+++ src/modules/user/user.validation.js
@@ -10,22 +10,24 @@ const createUserSchema = z.object({
   roleId: z.string('Role is required'),
 })
 
-const updateUserSchema = z
-  .object({
-    name: z.string().min(1, 'Name is required').optional(),
-    email: z.email('Invalid email address').optional(),
-    roleId: z.string('Role is required'),
-    password: z.string().refine((val) => {
+const updateUserSchema = z.object({
+  name: z.string().min(1, 'Name is required').optional(),
+  email: z.email('Invalid email address').optional(),
+  roleId: z.string('Role is required'),
+  password: z
+    .string()
+    .refine((val) => {
       if (!val) {
         return true // allow empty password (no change)
       }
+
       // min 8, has 1 uppercase, 1 number and special character
       return /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
         val,
       )
-    }, 'Password must be at least 8 characters long and include at least one uppercase letter, one number, and one special character'),
-  })
-  .optional()
+    }, 'Password must be at least 8 characters long and include at least one uppercase letter, one number, and one special character')
+    .optional(),
+})
 
 module.exports = {
   createUserSchema,
