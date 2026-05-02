63c753458541014f891acda5b86357046cdbbe15
Alfian Andre Ramadhan <andrealfian69699@gmail.com>
2026-05-02 12:57:28 +0700

    chore: update environment variables for consistency; improve xss middleware sanitization; adjust TypeScript configuration

--- src/middleware/xss.middleware.ts
+++ src/middleware/xss.middleware.ts
@@ -3,7 +3,7 @@ import xss from 'xss'
 
 const sanitizeInput = (obj: any): any => {
   try {
-    if (typeof obj !== 'object') {
+    if (typeof obj !== 'object' || obj === null) {
       if (typeof obj === 'string') {
         return xss(obj)
       } else {
@@ -28,16 +28,24 @@ const xssMiddleware = (req: Request, res: Response, next: NextFunction) => {
     // Leave markdown field unsanitized
     const markdown = req.body.markdown
     delete req.body.markdown
-    req.body = sanitizeInput(req.body)
+    sanitizeInput(req.body)
     req.body.markdown = markdown
   } else {
     req.body = sanitizeInput(req.body)
   }
-  req.query = sanitizeInput(req.query)
-  req.params = sanitizeInput(req.params)
-  req.headers = sanitizeInput(req.headers)
-  req.cookies = sanitizeInput(req.cookies)
-  req.url = sanitizeInput(req.url)
+
+  if (req.query) {
+    sanitizeInput(req.query)
+  }
+  if (req.params) {
+    sanitizeInput(req.params)
+  }
+  if (req.cookies) {
+    sanitizeInput(req.cookies)
+  }
+  if (req.url) {
+    sanitizeInput(req.url)
+  }
 
   // Sanitize responses
   res.locals.safeOutput = sanitizeInput(res.locals.safeOutput)
